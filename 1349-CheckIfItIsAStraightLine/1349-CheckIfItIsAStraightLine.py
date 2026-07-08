bd8ab71983f329778a54c472ec3ce58f435c4ef9
Zainul Abideen A <zainul4518@gmail.com>
2026-07-08 20:56:13 +0530

    Create 1349-CheckIfItIsAStraightLine

---
+++ 1349-CheckIfItIsAStraightLine/1349-CheckIfItIsAStraightLine.py
@@ -0,0 +1,16 @@
+# Last updated: 08/07/2026, 20:56:18
+from typing import List
+
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        x0, y0 = coordinates[0]
+        x1, y1 = coordinates[1]
+
+        dx = x1 - x0
+        dy = y1 - y0
+
+        for x, y in coordinates[2:]:
+            if (x - x0) * dy != (y - y0) * dx:
+                return False
+
+        return True
