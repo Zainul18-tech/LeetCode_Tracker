60b6870163df7674c06721dde702a0b4fdb87997
Zainul Abideen A <zainul4518@gmail.com>
2026-07-08 20:57:40 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,28 @@
+// Last updated: 08/07/2026, 20:57:45
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(char ch: s.toCharArray()){
+            if(ch =='(' || ch == '[' || ch == '{'){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+
+                if(ch==')' && top!= '('){
+                    return false;
+                }
+                if(ch ==']' && top!= '['){
+                    return false;
+                }
+                if (ch == '}' && top!= '{'){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
