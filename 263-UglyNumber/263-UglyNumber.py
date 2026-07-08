1edee3567b432b9161f94ffb56dd2e327dc2aa47
Zainul Abideen A <zainul4518@gmail.com>
2026-07-08 20:56:43 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.py
@@ -0,0 +1,11 @@
+# Last updated: 08/07/2026, 20:56:48
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        for p in [2, 3, 5]:
+            while n % p == 0:
+                n //= p
+
+        return n == 1
