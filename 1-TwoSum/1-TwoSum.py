9bb688c46a416b6235aac705403de502fbc4f23c
Zainul Abideen A <zainul4518@gmail.com>
2026-07-08 20:57:44 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,9 @@
+# Last updated: 08/07/2026, 20:57:49
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_set = {}
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in hash_set:
+                return [hash_set[diff],i]
+            hash_set[n] = i    
