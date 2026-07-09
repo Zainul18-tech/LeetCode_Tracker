46ffc4e6902f7d61589ba353f2a100b65ea0fde7
Zainul Abideen A <zainul4518@gmail.com>
2026-07-09 10:37:42 +0530

    Create 229-MajorityElementIi

---
+++ 229-MajorityElementIi/229-MajorityElementIi.java
@@ -0,0 +1,24 @@
+// Last updated: 09/07/2026, 10:37:47
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        Map<Integer, Integer> elementCountMap = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            elementCountMap.put(nums[i], elementCountMap.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        List<Integer> majorityElements = new ArrayList<>();
+        int threshold = nums.length / 3;
+        
+        for (Map.Entry<Integer, Integer> entry : elementCountMap.entrySet()) {
+            int element = entry.getKey();
+            int count = entry.getValue();
+            
+            if (count > threshold) {
+                majorityElements.add(element);
+            }
+        }
+        
+        return majorityElements;
+    }
+}
