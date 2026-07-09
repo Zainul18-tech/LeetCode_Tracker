15e6e45c4826f6dbe6f53883fbda21c7234403a1
Zainul Abideen A <zainul4518@gmail.com>
2026-07-09 10:36:13 +0530

    Create 1833-FindTheHighestAltitude

---
+++ 1833-FindTheHighestAltitude/1833-FindTheHighestAltitude.java
@@ -0,0 +1,16 @@
+// Last updated: 09/07/2026, 10:36:18
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int n=gain.length,index=0,max=0;
+        int[]arr=new int[n+1];
+        arr[index++]=0;
+        for(int val:gain){
+            arr[index]=arr[index-1]+val;
+            if(arr[index]>max){
+                max=arr[index];
+            }
+            index++;
+        }
+        return max;
+    }
+}
