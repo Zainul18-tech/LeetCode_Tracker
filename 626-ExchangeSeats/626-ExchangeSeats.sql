d948002cd3ed53a3eb7241c4ad9350229c947d72
Zainul Abideen A <zainul4518@gmail.com>
2026-07-09 10:37:05 +0530

    Create 626-ExchangeSeats

---
+++ 626-ExchangeSeats/626-ExchangeSeats.sql
@@ -0,0 +1,15 @@
+-- Last updated: 09/07/2026, 10:37:09
+SELECT
+    CASE
+        WHEN id % 2 = 1
+             AND id + 1 IN (SELECT id FROM Seat)
+        THEN id + 1
+
+        WHEN id % 2 = 0
+        THEN id - 1
+
+        ELSE id
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
