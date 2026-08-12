e3543b8c2411141fac50a2a3db12e74ce5191a75
Zainul Abideen A <zainul4518@gmail.com>
2026-08-12 11:30:36 +0530

    Create 4353-LargestIntegerWithGivenDigitSum

---
+++ 4353-LargestIntegerWithGivenDigitSum/4353-LargestIntegerWithGivenDigitSum.py
@@ -0,0 +1,17 @@
+# Last updated: 12/08/2026, 11:31:09
+class Solution:
+    def largestInteger(self, n: int, s: int) -> int:
+        if s > 9 * n:
+            return -1
+
+        if s == 0 :
+            return 0
+
+        ans = []
+
+        for _ in range(n):
+            digit = min(9, s)
+            ans.append(str(digit))
+            s -= digit 
+
+        return int("".join(ans))
