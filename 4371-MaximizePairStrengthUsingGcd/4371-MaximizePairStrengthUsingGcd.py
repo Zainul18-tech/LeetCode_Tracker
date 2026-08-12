8e324b5f1ba4f8f8b145e4074374e86afc0e3247
Zainul Abideen A <zainul4518@gmail.com>
2026-08-12 11:30:26 +0530

    Create 4371-MaximizePairStrengthUsingGcd

---
+++ 4371-MaximizePairStrengthUsingGcd/4371-MaximizePairStrengthUsingGcd.py
@@ -0,0 +1,11 @@
+# Last updated: 12/08/2026, 11:30:59
+class Solution:
+    def maxPairStrength(self, nums: list[int]) -> int:
+        ans = 0
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i+1, n):
+                g = gcd(nums[i], nums[j])
+                ans = max(ans, (nums[i] * nums[j]) // (g * g))
+        return ans
