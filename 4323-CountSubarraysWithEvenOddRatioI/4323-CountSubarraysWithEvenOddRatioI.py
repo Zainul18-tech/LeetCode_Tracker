a1945eff66dcc96bf0da488df628dd76912ba6cb
Zainul Abideen A <zainul4518@gmail.com>
2026-08-12 11:31:27 +0530

    Create 4323-CountSubarraysWithEvenOddRatioI

---
+++ 4323-CountSubarraysWithEvenOddRatioI/4323-CountSubarraysWithEvenOddRatioI.py
@@ -0,0 +1,19 @@
+# Last updated: 12/08/2026, 11:32:00
+class Solution:
+    def countRatioSubarrays(self, nums: list[int], a: int, b: int) -> int:
+        n = len(nums)
+        ans = 0
+
+        for i in range(n):
+            even = 0
+            odd = 0
+            for j in range(i, n):
+                if nums[j] % 2 == 0:
+                    even += 1
+                else:
+                    odd += 1
+
+                if odd > 0 and even * b <= odd * a:
+                    ans += 1
+        return ans
+                    
